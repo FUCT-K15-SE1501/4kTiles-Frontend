7d2217cdd74e54cba10c998f0d1249246de8548a
lexua <lexua@TRAINEX-PL>
2022-01-23 16:10:40 +0700

    Add project files.

---
+++ Core/Functions/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _4kTiles_Frontend.Core.Functions
+{
+    internal class ObservableObject: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
