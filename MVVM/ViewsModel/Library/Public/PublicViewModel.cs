ca9688fb8891089e1271a63add79a8a69e7e971a
Trainex <50549524+tunbk1002@users.noreply.github.com>
2022-03-17 15:22:22 +0700

    create sample for filtering list view (#10)
    Signed-off-by: tulx <lexuantu1002@gmail.com>

--- MVVM/ViewsModel/Library/Public/PublicViewModel.cs
+++ MVVM/ViewsModel/Library/Public/PublicViewModel.cs
@@ -13,6 +13,8 @@ namespace _4kTiles_Frontend.MVVM.ViewsModel.Library.Public
         #region fields
         private object _currentView;
         private List<SongOverviewViewModel> _songsList;
+        private string _searchText;
+        private int _selectedIndex;
         #endregion
 
         #region properties
@@ -37,6 +39,31 @@ namespace _4kTiles_Frontend.MVVM.ViewsModel.Library.Public
                 OnPropertyChanged();
             }
         }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                _selectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NextCommand { get; set; }
+        public RelayCommand PrevCommand { get; set; }
+
+        public RelayCommand SearchCommand { get; set; }
         #endregion
 
         public PublicViewModel()
@@ -46,13 +73,35 @@ namespace _4kTiles_Frontend.MVVM.ViewsModel.Library.Public
 
             Task.Run(async () =>
             {
-                await GetList();
+                SelectedIndex = 1;
+                await GetList(SelectedIndex);
+            });
+
+            NextCommand = new RelayCommand(async o =>
+            {
+                if (SongsList.Count > 0)
+                {
+                    SelectedIndex += 1;
+                    await GetList(page: SelectedIndex, searchText: SearchText);
+                }
+            });
+            PrevCommand = new RelayCommand(async o =>
+            {
+                if (SongsList.Count > 0)
+                {
+                    SelectedIndex -= 1;
+                    await GetList(page: SelectedIndex, searchText: SearchText);
+                }
+            });
+            SearchCommand = new RelayCommand(async o =>
+            {
+                await GetList(page: 1, searchText: SearchText);
             });
         }
 
-        public async Task<bool> GetList()
+        public async Task<bool> GetList(int page = 1, string searchText = "")
         {
-            var songsResponse = await Client.RequestAsync<List<SongOverviewModel>>("Library");
+            var songsResponse = await Client.RequestAsync<List<SongOverviewModel>>($"Library/search?pageNumber={page}&pageSize={4}&searchString={searchText}");
 
             if (songsResponse == null || songsResponse.IsError)
             {
@@ -60,7 +109,7 @@ namespace _4kTiles_Frontend.MVVM.ViewsModel.Library.Public
             }
 
             var list = new List<SongOverviewViewModel>();
-            foreach(var song in songsResponse.Data)
+            foreach (var song in songsResponse.Data)
             {
                 var newSongItem = new SongOverviewViewModel
                 {
