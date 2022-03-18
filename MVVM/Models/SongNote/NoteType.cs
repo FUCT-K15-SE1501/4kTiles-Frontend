8548c92e5e9e76bff8b8ca4c26163fe26401398e
Trainex <50549524+tunbk1002@users.noreply.github.com>
2022-03-18 13:23:29 +0700

    fix typo in SongNote model

--- MVVM/Models/SongNote/NoteType.cs
+++ MVVM/Models/SongNote/NoteType.cs
@@ -8,7 +8,7 @@ namespace _4kTiles_Frontend.MVVM.Models.SongNote
 {
     public class NoteType
     {
-        public float Volumn { get; set; } = 1;
+        public float Volume { get; set; } = 1;
         public float Delay { get; set; } = 0;
         public int MidiKey { get; set; } = 72;
         public float Length { get; set; } = 0;
