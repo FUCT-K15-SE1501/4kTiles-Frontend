15674eb94f171aff218e83d2c4955b9412740ff8
Trainex <50549524+tunbk1002@users.noreply.github.com>
2022-03-18 10:56:19 +0700

    create empty row feature (#11)

--- MVVM/Models/SongNote/Note.cs
+++ MVVM/Models/SongNote/Note.cs
@@ -9,7 +9,7 @@ namespace _4kTiles_Frontend.MVVM.Models.SongNote
     public class Note
     {
         // relative to current row [0,1,2,3]
-        public int Position { get; set; } = 0;
+        public int Position { get; set; } = -1;
         public List<NoteType> noteType { get; set; } = new()
         {
             new()
